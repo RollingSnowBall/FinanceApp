e91bd7833a1a6d892af7c7a95f935d10e0e9dfee
JUNO <jrlee0701@kakao.com>
2022-06-07 21:08:21 +0900

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by JUNO on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
