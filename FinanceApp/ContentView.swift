2d983f530bf4b0a0d54f315e1f22a290f7860824
JUNO <jrlee0701@kakao.com>
2022-06-08 22:35:16 +0900

    SwiftUI

--- FinanceApp/ContentView.swift
+++ FinanceApp/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection: Tab = .asset
+    
+    enum Tab {
+        case asset
+        case recommend
+        case alert
+        case setting
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            AssetView()
+                .tabItem {
+                    Image(systemName: "dollarsign.circle.fill")
+                    Text("자산")
+                }
+                .tag(Tab.asset)
+            
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("추천")
+                }
+                .tag(Tab.recommend)
+            
+            Color.yellow
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("알람")
+                }
+                .tag(Tab.alert)
+            
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+        }.onAppear {
+            let tabBarAppearance: UITabBarAppearance = UITabBarAppearance()
+                tabBarAppearance.configureWithDefaultBackground()
+                UITabBar.appearance().standardAppearance = tabBarAppearance
+
+                if #available(iOS 15.0, *) {
+                    UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+                }
+        }
     }
 }
 
---
+++ FinanceApp/Entities/AssetBanner.swift
@@ -0,0 +1,14 @@
+//
+//  AssetBanner.swift
+//  FinanceApp
+//
+//  Created by JUNO on 2022/06/08.
+//
+
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
